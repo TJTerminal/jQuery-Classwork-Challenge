e4609ca608b7fb9441cdd9d62cedbb633790cc86
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2021-04-08 19:35:36 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+    const newHomes = [
+        {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
+        {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
+        {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
+        {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
+      ];
+
+    let removedHomes = [];
+
+    $('#addHome').removeClass('btn-danger').addClass('btn-success');
+    $(h1).addClass('text-center');
+
+    let $newLink = $('<br><br><a id="zillowLink" href="http://www.zillow.com">Visit Zillow.com</a>');
+    $()
+})
